003a38ca8b7863c9dacb2ca18a70823da25fb1d3
beatbox8 <beatboxstudio8@gmail.com>
2025-04-28 12:04:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(cors({
-  origin: 'https://beat-box-frontend.onrender.com', // Replace with your frontend URL
+  origin: 'http://localhost:3000', // Replace with your frontend URL
   credentials: true, // Allow cookies to be sent with requests
 }));
 app.use(express.json());
