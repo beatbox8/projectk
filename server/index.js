db59e039214f2d092913690c476e571c7ae2f44f
beatbox8 <beatboxstudio8@gmail.com>
2025-04-28 22:26:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ const { protectRoute } = require('./middleware/authUser.middleware');
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
+ 
 app.use(cors({
-  origin: 'https://fluffy-fudge-704cd6.netlify.app', // Replace with your frontend URL
+  origin: ['https://fluffy-fudge-704cd6.netlify.app','http://localhost:3000'], // Replace with your frontend URL
   credentials: true, // Allow cookies to be sent with requests
 }));
 app.use(express.json());
