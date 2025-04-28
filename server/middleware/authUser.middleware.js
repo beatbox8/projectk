ccfb007a887837d2da2e725e667d7bb01cd70174
beatbox8 <beatboxstudio8@gmail.com>
2025-04-28 22:58:56 +0530

    Update authUser.middleware.js

--- server/middleware/authUser.middleware.js
+++ server/middleware/authUser.middleware.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const protectRoute = async (req, res, next) => {
-    const token = req.cookies.token;
-    console.log("Token from cookie:", token);
+    // const token = req.cookies.token;
+    // console.log("Token from cookie:", token);
 
-    if (!token) {
-        return res.status(401).json({ success: false, message: "Unauthorized User" });
-    }
+    // if (!token) {
+    //     return res.status(401).json({ success: false, message: "Unauthorized User" });
+    // }
 
     try {
-        console.log("JWT_SECRET:", process.env.JWT_SECRET);
+        // console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-        console.log("Decoded token:", decoded);
-        req.user = decoded;
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+        // console.log("Decoded token:", decoded);
+        // req.user = decoded;
 
         next();
     } catch (error) {
