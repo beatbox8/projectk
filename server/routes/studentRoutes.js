3a9012fcb895d421d3b57652d997e9eb27d5f609
beatbox8 <beatboxstudio8@gmail.com>
2025-04-28 22:41:05 +0530

    Update studentRoutes.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -3,10 +3,10 @@ const studentController = require('../controllers/studentController');
 const authMiddleware = require('../middleware/authUser.middleware')
 const router = express.Router();
 
-router.get('/',studentController.getAllStudents);
-router.post('/', studentController.addStudent);
-router.get('/:id', studentController.getStudentById);
-router.put('/:id', studentController.updateStudent);
-router.delete('/:id', studentController.deleteStudent);
+router.get('/',authMiddleware.protectRoute,studentController.getAllStudents);
+router.post('/',authMiddleware.protectRoute, studentController.addStudent);
+router.get('/:id',authMiddleware.protectRoute, studentController.getStudentById);
+router.put('/:id',authMiddleware.protectRoute, studentController.updateStudent);
+router.delete('/:id',authMiddleware.protectRoute, studentController.deleteStudent);
 
 module.exports = router;
