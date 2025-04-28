43f96cebcdc9e83830ecb73883599509f40e27fe
beatbox8 <beatboxstudio8@gmail.com>
2025-04-28 23:35:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import BeatboxDanceStudio from './components/home';
 import SignIn from './components/signIn';
 import Dashboard from './components/dashboard';
@@ -9,18 +9,21 @@ import Transactions from './components/transaction';
 import Reminders from './components/reminders';
 import Logout from './components/Logout';
 import { Toaster } from 'react-hot-toast';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<BeatboxDanceStudio />} />
-          <Route path="/signin" element={<SignIn/>} />
+          {/* Redirect from "/" to "/home" */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<BeatboxDanceStudio />} />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/data" element={<Data />} />
           <Route path="/transactions" element={<Transactions />} />
           <Route path="/reminders" element={<Reminders />} />
-          <Route path="/logout" element={<Logout/>}/>
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </BrowserRouter>
       <Toaster />
